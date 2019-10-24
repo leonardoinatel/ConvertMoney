62e7dc1a539dfa730599297a2394fb8c7f384a0a
leonardodamasceno8 <leonardodamasceno8@users>
2019-10-24 18:02:57 -0300

    plotly comentado

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //homemaeAPI
 const api = require('./lib/api.bcb')
 
+//API chart
+// var plotly = require('plotly')('costaleo122','EXb91ASPhl2DI1BAnrgR');
+
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -27,14 +30,23 @@ app.get('/', async(req,res) => {
 
 app.get('/cotacao', async (req, res) => { //incio da unidade
     const { cotacao, quantidade } = (req.query);
-    console.log("index.js",req.query)
 
     if(cotacao && quantidade){
         db = await dbcon
         const data = api.getToday()
-        await db.run(`INSERT into cotacao_dolar(cotacao,data) values('${cotacao}','${data}')`)
-
         const conversao = convert.convert(cotacao, quantidade)
+
+        await db.run(`INSERT into cotacao_dolar(cotacao,quantidade,conversao,data) 
+                        VALUES('${cotacao}','${quantidade}','${conversao}','${data}')`)
+
+        // const historico_moeda = await db.all('select * from cotacao_dolar;')    
+        // let i = 0;
+        // let cotacao = []
+        // const valorUnitario = historico_moeda.forEach( element => {
+        //     let cotacao[i] = element.cotacao
+        //     console.log(element.conversao)
+        // })
+        // console.log(valorUnitario)
         res.render('cotacao', {
             error: false,
             cotacao: convert.toMoney(cotacao),
@@ -42,13 +54,42 @@ app.get('/cotacao', async (req, res) => { //incio da unidade
             conversao: convert.toMoney(conversao)
         })
     }else{
-        console.log("testando",req.body)
         res.render('cotacao', {
             error: 'valores invalidos, você será redirecinado a pagina inicial em 4 segundos',
         })
     }
-}) //fim da unidade
+}) 
+
+//fim da unidade
 //unidade nao pode ser quebrada, isso define a unidade
+// var data = [
+//     {
+//         x: [0, 1, 2],
+//         y: [6, 10, 2],
+//         error_y: {
+//         type: "data",
+//         array: [1, 2, 3],
+//         visible: true
+//         },
+//         type: "scatter"
+//     }
+// ];
+
+// plotly.plot('myDiv', data, {}, {showSendToCloud: true});
+
+// var graphOptions = {filename: "basic-error-bar", fileopt: "overwrite"};
+// plotly.plot(data, graphOptions, function (err, msg) {
+//     console.log(msg);
+// });
+
+
+// const init = async() => {
+//         const db = await dbcon
+//         await db.run('create table if not exists cotacao_dolar(id INTEGER PRIMARY KEY, cotacao INTEGER, quantidade INTEGER, conversao INTEGER, data DATE);')
+        
+//         //await db.run('drop table cotacao_dolar')
+//     }
+// init()
 
 app.listen(port , err => {
     if(err){
@@ -58,4 +99,4 @@ app.listen(port , err => {
     }
 })
 
-//7:32
+
